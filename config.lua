ada170033465296fb167deae131228832681c2b9
Adam Biegert <118407273+AdamaStarkN7@users.noreply.github.com>
2025-03-09 17:16:16 -0600

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,99 @@
+Config = Config or {}
+
+---EF-Blips
+Config.blipsShow = true
+
+Config.Locations = {
+-- Example
+
+-- [[
+-- [0] = {
+--     vector = vector3(0, 0, 0),
+--     text = "Blip Name",
+--     color = 3,
+--     sprite = Blip ID,
+--     scale = size,
+-- }, ]]
+
+--- add more
+[1] = {
+    vector = vector3(538.32, -183.18, 54.47),
+    text = "Repair Station",
+    color = 1,
+    sprite = 446,
+    scale = 0.5,
+},
+
+[2] = {
+    vector = vector3(-778.01, 4965.29, 209.68),
+    text = "Hunting Zone",
+    color = 5,
+    sprite = 432,
+    scale = 0.5,
+},
+
+[3] = {
+    vector = vector3(-72.05, 6252.36, 31.09),
+    text = "Chicken Factory",
+    color = 26,
+    sprite = 273,
+    scale = 0.5,
+},
+
+[4] = {
+    vector = vector3(440.1, -983.22, 30.71),
+    text = "Mission Row Police Department",
+    color = 38,
+    sprite = 60,
+    scale = 0.9,
+},
+
+[5] = {
+    vector = vector3(-580.76, -407.57, 35.18),
+    text = "Del Perro Police Department",
+    color = 38,
+    sprite = 60,
+    scale = 0.9
+},
+
+[6] = {
+    vector = vector3(-450.34, -340.43, 34.5),
+    text = "Mount Zonah Medical Center",
+    color = 29,
+    sprite = 61,
+    scale = 0.9,
+},
+
+[7] = {
+    vector = vector3(1810.32, 3668.6, 34.25),
+    text = "Los Santos County Sheriff\'s Office",
+    color = 47,
+    sprite = 60,
+    scale = 0.9,
+},
+
+[8] = {
+    vector = vector3(1677.08, 3662.0, 35.35),
+    text = "Sandy Shores Clinic",
+    color = 29,
+    sprite = 61,
+    scale = 0.9,
+},
+
+[9] = {
+    vector = vector3(-17.71, -94.41, 57.03),
+    text = "Digital Den",
+    color = 27,
+    sprite = 521,
+    scale = 0.9,
+},
+
+[10] = {
+    vector = vector3(241.33, 225.45, 106.29),
+    text = "Paycheck",
+    color = 25,
+    sprite = 500,
+    scale = 0.9,
+},
+
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'blastersuraj'
+description 'Blip Creator Free For QBCore and ESX'
+version '1.0'
+discord 'https://discord.gg/WbDp5GQ45t'
+scriptname 'ef-blips'
+
+
+
+shared_script 'config.lua'
+client_script 'efblips.lua'
+
+
+escrow_ignore {
+    'config.lua',
+    'efblips.lua'
+}
+
+dependency '/assetpacks'
